b33e1b6e60223ea3969a858fec2243273b4976d5
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-05-09 02:33:31 +0300

    rename function

--- Task_begin_CreationFunction/main.cpp
+++ Task_begin_CreationFunction/main.cpp
@@ -7,7 +7,7 @@ unsigned long long Factorial(unsigned long long n)
 	while (n > 1) factorial *= --n;
 	return factorial;
 }
-double Exponenta(double a, int nn)
+double Power(double a, int nn)
 {
 	double N = 1;
 	if (nn < 0) 
@@ -30,8 +30,8 @@ void main()
 	cout << endl;
 
 	double number, exp;
-	cout << "Возведения в степень\nВведите Основание  : "; cin >> number;
+	cout << "Возведение в степень\nВведите Основание  : "; cin >> number;
 	cout << "Введите степень: "; cin >> exp;
-	cout << "Число " << number << " в степени " << exp << " равно " << Exponenta(number, exp) << endl;
+	cout << "Число " << number << " в степени " << exp << " равно " << Power(number, exp) << endl;
 
 }
