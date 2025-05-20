cc92a28bf5945325db5caf4894a6136f22661708
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-05-21 01:51:24 +0300

    oooo...Shift_r add.... End?

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -46,7 +46,7 @@ void ShiftLeft(char arr[], const int n, int shift_l);
 void ShiftRight(int arr[], const int n, int shift_r);
 void ShiftRight(double arr[], const int n, int shift_r);
 void ShiftRight(float arr[], const int n, int shift_r);
-void ShiftRight(char arr[], const int n);
+void ShiftRight(char arr[], const int n, int shift_r);
 
 
 void main()
